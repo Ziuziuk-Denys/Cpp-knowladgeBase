73c0ea011d4de555a817f8f196eb81acb78fc428
Denys <denys.ziuziuk.w@gmail.com>
2026-04-24 21:55:36 +0300

    Create Config.h

---
+++ Projects/Jarvis-Final-Project/include/Config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <string>
+
+const std::string GEMINI_API_KEY = "YOUR-API";
+const std::string ELEVEN_LABS_API_KEY = "YOUR-API";
+const std::string ELEVEN_LABS_VOICE_ID = "YOUR-API";
+
+#endif // CONFIG_H
